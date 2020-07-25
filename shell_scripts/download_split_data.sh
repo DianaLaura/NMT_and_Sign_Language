c97b4f3ccbff48d6ac67c60c2c9e69fd63c1d563
Diana Enggist <dianaenggist@Dianas-MacBook-Pro.local>
2020-07-25 03:14:57 +0200

    small changes; commented important sections out!

--- shell_scripts/download_split_data.sh
+++ shell_scripts/download_split_data.sh
@@ -8,23 +8,23 @@ base=$scripts/..
 
 #load data
 
-mkdir -p $storage/DGS_corpus_dirty
+#mkdir -p $storage/DGS_corpus_dirty
 
-mkdir -p $storage/DGS_corpus
+#mkdir -p $storage/DGS_corpus
 
-mkdir -p $storage/DGS_train
+#mkdir -p $storage/DGS_train
 
-mkdir -p $storage/DGS_test
+#mkdir -p $storage/DGS_test
 
-mkdir -p $storage/DGS_dev
+#mkdir -p $storage/DGS_dev
 
-mkdir -p $storage/Extracted_data
+#mkdir -p $storage/Extracted_data
 
-python3 ../data_loading_extraction/download_ilex.py --output_dir $storage/DGS_corpus_dirty/
+#python3 ../data_loading_extraction/download_ilex.py --output_dir $storage/DGS_corpus_dirty/
 
 #filter out files that are smaller than 3 kb, because they usually don't contain any usable data
 
-cp `find $storage/DGS_corpus_dirty -type f -size +3k` $storage/DGS_corpus
+#cp `find $storage/DGS_corpus_dirty -type f -size +3k` $storage/DGS_corpus
 
 #split into train and test set
 
@@ -40,13 +40,13 @@ devindex=`echo $testindex | awk 'END {print $1/2}' | awk '{print int ($1)}'`
 
 list=`ls $storage/DGS_corpus | ruby -e 'puts STDIN.readlines.shuffle(random: Random.new(42))'`
 
-cp `echo "$list" | head -$devindex | awk -v var="$storage" '{print var"/DGS_corpus/"$1}'` $storage/DGS_dev/
+cp -r `echo "$list" | head -$devindex | awk -v var="$storage" '{print var"/DGS_corpus/"$1}'` $storage/DGS_dev/
 
-cp `echo "$list" | tail -$trainindex | awk -v var="$storage" '{print var"/DGS_corpus/"$1}'` $storage/DGS_train/
+cp -r `echo "$list" | tail -$trainindex | awk -v var="$storage" '{print var"/DGS_corpus/"$1}'` $storage/DGS_train/
 
 devindex=`echo $devindex | awk 'END {print $1+1}'`
 
-cp `echo "$list" | head -$testindex | tail -$devindex |  awk -v var="$storage" '{print var"/DGS_corpus/"$1}'` $storage/DGS_test/
+cp -r `echo "$list" | head -$testindex | tail -$devindex |  awk -v var="$storage" '{print var"/DGS_corpus/"$1}'` $storage/DGS_test/
 
 
 
