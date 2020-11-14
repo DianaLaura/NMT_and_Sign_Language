9091b89f3810b38393968f971e961c799a6fbe55
DianaLaura <dianalaura.enggist@gmx.ch>
2020-11-14 21:12:48 +0100

    small changes on paths

--- shell_scripts/evaluate.sh
+++ shell_scripts/evaluate.sh
@@ -4,14 +4,13 @@ scripts=`dirname "$0"`
 base=$scripts/..
 
 
-translations=$4/translations
+translations=$1/translations
 
-mkdir -p $translations
 
-src=$1
-trg=$2
-spoken=$3
-data=$4/Extracted_data
+
+src=sign
+trg=de
+data=$1/test
 
 # cloned from https://github.com/bricksdont/moses-scripts
 MOSES=$base/tools/moses-scripts/scripts
@@ -22,27 +21,29 @@ num_threads=1
 ##########################################
 
 OMP_NUM_THREADS=$num_threads python -m sockeye.translate \
-				-i $4/Extracted_data/test.preprocessed.$src \
+				-i $1/Extracted_data/test.preprocessed.$src \
 				-o $translations/test.preprocessed.$model_name.$trg \
-				-m $4/models/$model_name \
+				-m $1/models/$model_name \
 				--beam-size 10 \
+				--use-cpu \
 				--length-penalty-alpha 1.0 \
-				--device-ids 0 \
 				--batch-size 16\
+
 
 
 
 #undo BPE
 
-cat $translations/test.bpe.$model_name.$spoken | sed 's/\@\@ //g' > $translations/test.truecased.$model_name.$spoken
-# undo truecasing
+cat $translations/test.preprocessed.$model_name.de | sed 's/\@\@ //g' > $translations/test.truecased.$model_name.de
+ #undo truecasing
 
-cat $translations/test.truecased.$model_name.$spoken| $MOSES/recaser/detruecase.perl > $translations/test.tokenized.$model_name.$spoken
 
-# undo tokenization
+cat $translations/test.truecased.$model_name.de| $MOSES/recaser/detruecase.perl > $translations/test.tokenized.$model_name.de
 
-cat $translations/test.tokenized.$model_name.$spoken | $MOSES/tokenizer/detokenizer.perl -l $spoken > $translations/test.$model_name.$spoken
-cat $translations/test.preprocessed.$model_name.sign > $translations/test.$model_name.sign
+#undo tokenization
+
+cat $translations/test.tokenized.$model_name.de | $MOSES/tokenizer/detokenizer.perl -l $spoken > $translations/test.$model_name.de
+#cat $translations/test.preprocessed.$model_name.sign > $translations/test.$model_name.sign
 # compute case-sensitive BLEU on detokenized data
 
-cat $translations/test.$model_name.$trg | sacrebleu $data/test.$trg
+cat $translations/test.$model_name.de | sacrebleu $data/test.de
--- shell_scripts/prepare_data_source_factors.sh
+++ shell_scripts/prepare_data_source_factors.sh
@@ -5,7 +5,7 @@ trg=de
 data=$1/Extracted_data
 base=$scripts/..
 
-mkdir -p $1/prepared_data_concat
+mkdir -p $1/prepared_data_source
 
 num_threads=1
 model_name=baseline
@@ -14,7 +14,7 @@ model_name=baseline
 OMP_NUM_THREADS=$num_threads python -m sockeye.prepare_data \
       --source $data/train.preprocessed.sign\
       --target $data/train.preprocessed.$trg \
-      --output $1/prepared_data_concat/data.version \
+      --output $1/prepared_data_source/data.version \
       --max-seq-len=200:100 \
       --no-bucketing \
       --source-factors $data/train.preprocessed.mouthings
